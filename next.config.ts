23a80ef96f99e7e8b14978c1af9264b97950a2a9
Lee Thornton <leethornton@Lees-MacBook-Air.local>
2026-09-16 22:05:23 +1000

    Allow Vercel Blob images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import { withPayload } from "@payloadcms/next/withPayload";
-import type { NextConfig } from "next";
+import { withPayload } from '@payloadcms/next/withPayload'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          'mrhm2hafenbkzvcq.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+}
 
-export default withPayload(nextConfig);
+export default withPayload(nextConfig)
